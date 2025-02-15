5b102a4953786d47df5a8cf8d681c82ba4c9acce
Rohid <rohidul209@gmail.com>
2025-02-15 23:18:19 +0600

    parse post using ai

---
+++ apps/web/src/app/api/tasks/scrape-posts/route.ts
@@ -0,0 +1,136 @@
+import { newsPublishers } from "@/constants/publishers";
+import { db, Prisma } from "@acme/db";
+import { openai } from "@ai-sdk/openai";
+import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
+import { generateObject } from "ai";
+import * as cheerio from "cheerio";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export const maxDuration = 120;
+
+export const POST = verifySignatureAppRouter(async (req: NextRequest) => {
+  const body = await req.json();
+  const { urls, publisherId } = await z
+    .object({
+      urls: z.array(z.string().url()).min(1).max(10),
+      publisherId: z.string(),
+    })
+    .parseAsync(body);
+
+  const publisher = newsPublishers.find(
+    (publisher) => publisher.id === publisherId,
+  );
+
+  if (!publisher) {
+    return NextResponse.json("Publisher not found!", { status: 404 });
+  }
+
+  try {
+    const results = await Promise.allSettled(urls.map(scrapeFeedItem));
+    const data = results
+      .map((result) => {
+        if (result.status === "fulfilled") {
+          return {
+            ...result.value,
+            language: publisher.language,
+            countryCode: publisher.countryCode,
+            sourceName: publisher.name,
+          } satisfies Prisma.PostCreateInput;
+        }
+        return null;
+      })
+      .filter((item) => !!item);
+
+    await db.post.createMany({
+      data,
+      skipDuplicates: true,
+    });
+
+    return new NextResponse("Success");
+  } catch (error) {
+    console.error(`Failed to scrape posts`, error);
+    return NextResponse.json(
+      {
+        message: `Failed to scrape posts`,
+        error,
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+});
+
+const scrapeFeedItem = async (
+  sourceUrl: string,
+): Promise<{
+  title: string;
+  content: string;
+  imageUrl?: string;
+  sourceUrl: string;
+}> => {
+  const exists = await db.post.findUnique({
+    where: { sourceUrl },
+  });
+
+  if (exists) {
+    throw new Error("Already scraped");
+  }
+
+  const res = await fetch(sourceUrl);
+  if (!res.ok) {
+    throw res.statusText;
+  }
+  const htmlText = await res.text();
+  const $ = cheerio.load(htmlText);
+
+  let title: string | undefined = $("title").text().trim();
+  if (!title) {
+    title = $(`meta[name="title"]`).attr("content")?.trim();
+  }
+
+  if (!title) {
+    throw new Error("Title not found!");
+  }
+
+  const metaOgImage = $(`meta[property="og:image"]`).attr("content");
+  const metaTwitterImage = $(`meta[name="twitter:image"]`).attr("content");
+  const imageUrl = metaOgImage ?? metaTwitterImage;
+
+  $("head").remove();
+  $("meta").remove();
+  $("script").remove();
+  $("style").remove();
+  $("noscript").remove();
+  $("iframe").remove();
+  $("aside").remove();
+  $("header").remove();
+  $("footer").remove();
+  $("nav").remove();
+  const body = $.html().trim();
+
+  const result = await generateObject({
+    model: openai("gpt-4o-mini"),
+    system:
+      "Extract the main points from the following HTML blog post and generate a concise summary in the same language as the original text. Ensure the summary captures key ideas, main arguments, and important conclusions while maintaining clarity and readability. Remove unnecessary HTML tags or metadata and focus only on the main content.",
+    schema: z.object({
+      summary: z
+        .string()
+        .describe(
+          "A concise summary extracted from the blog post, capturing key ideas and main arguments. Maximum 500 characters",
+        ),
+    }),
+    messages: [{ role: "user", content: body }],
+    maxTokens: 300,
+  });
+
+  const content = result.object.summary;
+
+  return {
+    sourceUrl,
+    title,
+    content,
+    imageUrl,
+  };
+};
--- apps/web/src/app/api/tasks/scrape-publisher/route.ts
+++ apps/web/src/app/api/tasks/scrape-publisher/route.ts
@@ -1,147 +1,55 @@
-import { chunkArray } from "@/lib/utils";
-import { NewsPublisher } from "@/types";
-import { db, Prisma } from "@acme/db";
-import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
-import { NextRequest, NextResponse } from "next/server";
-import * as htmlparser2 from "htmlparser2";
-import { parse } from "node-html-parser";
-import { z } from "zod";
 import { newsPublishers } from "@/constants/publishers";
-
-const getPageMetadata = async (link: string) => {
-  const res = await fetch(link);
-  const html = await res.text();
-  const $ = parse(html);
-  const language = $.querySelector("html")?.getAttribute("lang")?.trim();
-  const content = $.querySelector('meta[property="og:description"]')
-    ?.getAttribute("content")
-    ?.trim();
-  const authorName = $.querySelector('meta[name="author"]')
-    ?.getAttribute("content")
-    ?.trim();
-  const keywords = $.querySelector('meta[name="keywords"]')
-    ?.getAttribute("content")
-    ?.trim()
-    .split(",")
-    .map((keyword) => keyword.trim())
-    .filter(Boolean);
-  const title = $.querySelector('meta[property="og:title"]')
-    ?.getAttribute("content")
-    ?.trim();
-  const imageUrl = $.querySelector('meta[property="og:image"]')
-    ?.getAttribute("content")
-    ?.trim();
-
-  return {
-    language,
-    title,
-    content,
-    imageUrl,
-    authorName,
-    keywords,
-  };
-};
-
-const scrapeFeedItem = async (item: htmlparser2.DomUtils.FeedItem) => {
-  if (typeof item.link !== "string") {
-    console.log("Invalid Link", item.link);
-    throw new Error("Invalid Link");
-  }
-
-  const sourceUrl = item.link;
-
-  const exists = await db.post.findUnique({
-    where: { sourceUrl },
-  });
-
-  if (exists) {
-    throw new Error("Already scraped");
-  }
-
-  const pageMetadata = await getPageMetadata(sourceUrl);
-  const title = pageMetadata?.title || item.title;
-  const content = pageMetadata?.content || item.description;
-  const imageUrl = pageMetadata?.imageUrl ?? item.media?.[0]?.url;
-
-  if (!title || title.length >= 100) {
-    throw new Error("Invalid title");
-  }
-
-  if (!content || content.length > 500 || content.length < 50) {
-    throw new Error("Invalid content");
-  }
-
-  const newsItem = {
-    title,
-    content,
-    sourceUrl,
-    imageUrl,
-  };
-
-  return newsItem;
-};
-
-const scrapePublisherFeed = async (publisher: NewsPublisher) => {
-  const res = await fetch(publisher.rssFeedUrl);
-  const feedStr = await res.text();
-  const feed = htmlparser2.parseFeed(feedStr);
-
-  if (!feed) {
-    throw new Error("Failed to parse feed");
-  }
-
-  console.log(`${publisher.url} - TOTAL ${feed.items.length} ITEMS`);
-
-  const posts: Prisma.PostCreateInput[] = [];
-
-  const chunks = chunkArray(feed.items, 30);
-
-  for (let i = 0; i < chunks.length; i++) {
-    try {
-      const chunk = chunks[i];
-      const results = await Promise.allSettled(chunk.map(scrapeFeedItem));
-      results.forEach((result) => {
-        if (result.status === "fulfilled") {
-          posts.push({
-            ...result.value,
-            language: publisher.language,
-            countryCode: publisher.countryCode,
-            sourceName: publisher.name,
-          });
-        }
-      });
-    } catch (error: any) {}
-  }
-  console.log(`${publisher.url} - TOTAL ${posts.length} UNIQUE POSTS`);
-
-  return posts;
-};
-
-export const maxDuration = 120;
+import { qstashClient } from "@/lib/qstash-client";
+import { chunkArray } from "@/lib/utils";
+import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
+import * as htmlparser2 from "htmlparser2";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 export const POST = verifySignatureAppRouter(async (req: NextRequest) => {
   const body = await req.json();
   const { publisherId } = await z
     .object({ publisherId: z.string() })
     .parseAsync(body);
+
   const publisher = newsPublishers.find(
     (publisher) => publisher.id === publisherId,
   );
+
   if (!publisher) {
     return NextResponse.json("Publisher not found!", { status: 404 });
   }
 
   try {
-    const data = await scrapePublisherFeed(publisher);
+    const res = await fetch(publisher.rssFeedUrl);
+    const feedStr = await res.text();
+    const feed = htmlparser2.parseFeed(feedStr);
 
-    const post = await db.post.createMany({
-      data,
-      skipDuplicates: true,
-    });
+    if (!feed) {
+      throw new Error("Failed to parse feed");
+    }
 
-    return new NextResponse(
-      `Publisher "${publisher.id}" scrapped successfully. Total scrapped ${post.count} posts.`,
+    console.log(`${publisher.url} - TOTAL ${feed.items.length} ITEMS`);
+
+    const allUrls = feed.items
+      .map((item) => item.link)
+      .filter((link) => !!link) as string[];
+
+    const chunks = chunkArray(allUrls, 10);
+
+    await Promise.all(
+      chunks.map((urls) =>
+        qstashClient.publishJSON({
+          url: `https://sonkhipto.com/api/tasks/scrape-posts`,
+          body: {
+            publisherId: publisher.id,
+            urls,
+          },
+        }),
+      ),
     );
+
+    return new NextResponse("Success");
   } catch (error) {
     console.error(`Failed to scrape ${publisher.id}`, error);
     return NextResponse.json(
