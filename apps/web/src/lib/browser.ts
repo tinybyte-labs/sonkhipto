656d7bc2818d6b233dd207414ed77de1b0f0776e
Rohid <rohidul209@gmail.com>
2025-02-24 22:27:22 +0600

    #

--- apps/web/src/lib/browser.ts
+++ apps/web/src/lib/browser.ts
@@ -1,14 +1,15 @@
 import puppeteer, { type Browser } from "puppeteer-core";
-import chromium from "@sparticuz/chromium-min";
+import chrome from "chrome-aws-lambda";
 
 export const getBrowser = async (): Promise<Browser> => {
-  const executablePath = await chromium.executablePath(
-    "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar",
-  );
+  // const aws = process.env.AWS_LAMDA_FUNCTION_VERSION
+  // const executablePath = await chromium.executablePath(
+  //   "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar",
+  // );
   return puppeteer.launch({
-    executablePath,
-    args: chromium.args,
-    headless: "shell",
-    defaultViewport: chromium.defaultViewport,
+    executablePath: await chrome.executablePath,
+    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chrome.defaultViewport,
+    headless: true,
   });
 };
