93906e20c4f536af6079d25fc1916e1f4bd7d8a7
Rohid <rohidul209@gmail.com>
2025-03-03 22:12:43 +0600

    health check

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -3,6 +3,7 @@ import { scrapeLinks } from "./scrape-link";
 import { scrapeArtickle } from "./scrape-article";
 
 export const routes = async (fastify: FastifyInstance) => {
+  fastify.get("/", (req, reply) => reply.send("OK"));
   fastify.post("/scrape-links", scrapeLinks);
   fastify.post("/scrape-article", scrapeArtickle);
 };
